2f8a2d9c9f614495595040a8abc17cbf80ce2fe9
Breno Perucchi <bperucchi@gmail.com>
2020-09-11 03:08:01 -0300

    Update ocr_google.py

--- lib/signal/lib/ocr_google.py
+++ lib/signal/lib/ocr_google.py
@@ -1,9 +1,8 @@
 import re
 # import pdb
-
 def detect_text_uri(uri):
     """Detects text in the file located in Google Cloud Storage or on the Web."""
-    
+
     from google.cloud import vision
     client = vision.ImageAnnotatorClient()
     image = vision.types.Image()
