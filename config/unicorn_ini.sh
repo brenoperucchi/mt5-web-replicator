67a868a464c49f6507797bdba159664367e3b5f2
Breno Perucchi <bperucchi@gmail.com>
2020-10-09 02:57:10 -0300

    deploy fix

--- config/unicorn_ini.sh
+++ config/unicorn_ini.sh
@@ -12,10 +12,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/<app_name>/current
+APP_ROOT=/home/bperucchi/app/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=<user>
+AS_USER=bperucchi
 set -u
 
 OLD_PIN="$PID.oldbin"
