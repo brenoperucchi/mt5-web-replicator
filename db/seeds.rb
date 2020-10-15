64b377acdb5cb200056ba9b589064234f68a6816
Breno Perucchi <bperucchi@gmail.com>
2020-10-15 15:19:00 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 store = Store.create(name:'Store 1', active_at: DateTime.now)
-store.traces.create(name: 'M15 Signals Premium', name_id:'-481414224', active_at: DateTime.now, telegram_option:'query_name_id', telegram_image:true)
-store.traces.create(name: 'Swing Trading', name_id:'-481414224', active_at: DateTime.now, telegram_option:'query_name', telegram_image:false)
+store.traces.create(name: 'M15 Signals Premium', name_id:'-1001222448337', active_at: DateTime.now, telegram_option:'query_name_id', telegram_image:true)
+store.traces.create(name: 'Swing Trading ViP', name_id:'-1001159029077', active_at: DateTime.now, telegram_option:'query_name', telegram_image:false)
 if Rails.env.development?
   store.traces.create(name: 'RoboSignal', name_id:'-481414224', active_at: nil, telegram_option:'query_name', telegram_image:true)
   store.traces.create(name: 'Perucchi Inc', name_id:'-340961920', active_at: nil, telegram_option:'query_name_id', telegram_image:false)
