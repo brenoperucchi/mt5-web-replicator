9ac80cdf1c04570718d188f0d7a8bc75a5d7396e
Breno Perucchi <bperucchi@gmail.com>
2020-10-23 11:15:56 -0300

    API V2

---
+++ app/controllers/api/v2/base.rb
@@ -0,0 +1,10 @@
+module API
+  module V2
+    class Base < Grape::API
+      mount API::V2::Orders
+      # mount API::V1::Slaves
+      # mount API::V1::Traces
+      # mount API::V1::Stores
+    end
+  end
+end
