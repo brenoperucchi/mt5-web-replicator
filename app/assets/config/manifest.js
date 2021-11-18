6a02df1dc69cd50c9509c4b024f0a4a011e7cf37
Breno Perucchi <bperucchi@gmail.com>
2021-11-17 21:00:05 -0300

    fix deploy

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
 //= link administrate/application.css
 //= link administrate/application.js
-//= link administrate-field-tag/application.js
