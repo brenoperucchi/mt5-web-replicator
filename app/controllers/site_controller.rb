5527785d23c659689c649307eaf6cd5ef7a2672f
Breno Perucchi <bperucchi@gmail.com>
2022-11-14 17:44:46 -0300

    improving land page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,8 +1,21 @@
 class SiteController < ApplicationController
 	layout "saasley"
+	before_action :set_locale
 
 	def index
-		
+
+
+	end
+
+
+	def set_locale
+		locale = params[:locale]
+		case locale
+		when "en"
+			I18n.locale = :en
+		else
+			I18n.locale = 'pt-BR'
+		end
 	end
 
 end
