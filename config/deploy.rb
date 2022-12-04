825410a113809b367b23972a3613e338485f9be0
Breno Perucchi <bperucchi@gmail.com>
2022-12-04 18:09:02 -0300

    fixing

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,16 +30,16 @@ before 'deploy:publishing', 'deploy:stop'
 after 'deploy:publishing', 'deploy:start'
 
 
-namespace :bundler do
-  before 'bundler:install', :config
-    desc 'bundle config options'
-    task :config do
-      on roles(:all), in: :groups, limit: 3, wait: 10 do
-      # Required for pg gem to be installed
-      execute 'bundle config build.pg --with-pg-config=/usr/pgsql-13/bin/pg_config'
-    end
-  end
-end
+# namespace :bundler do
+#   before 'bundler:install', :config
+#     desc 'bundle config options'
+#     task :config do
+#       on roles(:all), in: :groups, limit: 3, wait: 10 do
+#       # Required for pg gem to be installed
+#       execute 'bundle config build.pg --with-pg-config=/usr/pgsql-13/bin/pg_config'
+#     end
+#   end
+# end
 
 namespace :deploy do
   task :add_default_hooks do
