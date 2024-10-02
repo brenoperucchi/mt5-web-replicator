9bfa9b9c17ddaf57320d239578344962ee733c2f
Breno Perucchi <bperucchi@gmail.com>
2024-10-02 16:32:24 -0300

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,10 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+if ENV.fetch("RAILS_ENV", "development") == "development"
+  worker_timeout 3600 
+  threads 1,2
+end
 port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/puma.pid" }
